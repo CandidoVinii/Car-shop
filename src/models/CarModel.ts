5c2adfbc11e22f58dfb9003e0fa29aa5a21cc02b
root <root@SEN002157.sensedia.local>
2022-10-24 22:10:30 -0300

    feat: criada model generica e model para oo tipo carro

---
+++ src/models/CarModel.ts
@@ -0,0 +1,21 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from "../interfaces/ICar";
+import MongoModel from "./MongoModel";
+
+const CarModelSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class CarModel extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('CarModel', CarModelSchema)) {
+    super(model)
+  }
+};
+
+export default CarModel;
